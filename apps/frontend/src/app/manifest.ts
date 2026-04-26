264d94556348b39ef46598b168368cb618e19ee1
Rodrigo Paredes <rodrigo.paredes@example.com>
2026-04-25 22:44:14 -0400

    fix: add correct sized PWA icons (192x192 and 512x512)

--- apps/frontend/src/app/manifest.ts
+++ apps/frontend/src/app/manifest.ts
@@ -12,13 +12,13 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: 'portrait',
     icons: [
       {
-        src: '/logos/logo-negro.png',
+        src: '/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any maskable',
       },
       {
-        src: '/logos/logo-negro.png',
+        src: '/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any maskable',
